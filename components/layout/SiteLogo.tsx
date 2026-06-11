0c06d9edb7150379a470c4fa567e0f1c7d5241eb
muhtalhaa <muh.talha06@gmail.com>
2026-06-11 22:48:39 +0700

    logo update 2

--- components/layout/SiteLogo.tsx
+++ components/layout/SiteLogo.tsx
@@ -1,22 +1,26 @@
 import Image from "next/image";
 import Link from "next/link";
 
+const LOGO_ASPECT_RATIO = 1575 / 999;
+
 interface SiteLogoProps {
   height?: number;
   className?: string;
 }
 
 export function SiteLogo({ height = 32, className = "" }: SiteLogoProps) {
+  const width = Math.round(height * LOGO_ASPECT_RATIO);
+
   return (
     <Link href="/" className={`inline-block pixel-transition ${className}`}>
       <Image
         src="/images/logo.png"
         alt="menaRun"
-        width={height}
+        width={width}
         height={height}
         data-pixel="true"
-        className="pixel-render w-auto"
-        style={{ height: `${height}px` }}
+        className="pixel-render"
+        style={{ height: `${height}px`, width: `${width}px` }}
         priority
       />
     </Link>
